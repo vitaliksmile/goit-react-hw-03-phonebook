0e2af8d8c9c872609ab930a3736aeaa86a1d8301
vitaliksmile <vitaliksmile777@gmail.com>
2022-11-01 16:59:38 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,22 @@ import s from './App.module.css';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contact = localStorage.getItem('contacts');
+    const contactParse = JSON.parse(contact);
+    if (contactParse) {
+      this.setState({ contacts: contactParse });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   handleChangeForm = e => {
     const { name, value } = e.target;
 
